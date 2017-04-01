f065e9d3c30ac332623eb53dccb070b255817dfe
Nathanial S. Lubitz <nate.lubitz@gmail.com>
2017-03-31 19:05:58 -0500

    Just commiting again

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+def index
+@content_first = "This is a test string";
+end
+
+def new
+  
+end
+
+def create
+  
+end
+
+def edit
+  
+end
+
+def update
+  
+end
+
+def show
+  
+end
+
+def destroy
+  
+end
+
+end
