4cf345be09b3d18c46fd8c156a0b4d69abceabdf
Nathanial S. Lubitz <nate.lubitz@gmail.com>
2017-04-01 12:37:58 -0500

    Fixed comments not adding due to permissions

--- app/admin/posttag.rb
+++ app/admin/posttag.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Posttag do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :postID, :tagID
 #
 # or
 #
