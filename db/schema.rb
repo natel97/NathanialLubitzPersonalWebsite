d7541f03f44f2670abd77139cfdbdc8b2af10b43
Nathanial S. Lubitz <nate.lubitz@gmail.com>
2017-04-01 22:14:56 -0500

    fixed Database

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20170401194657) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
+    t.string   "nname"
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
   end
