b7327691b63e977a5e52b1e4689495a4e4d03a21
Nathanial S. Lubitz <nate.lubitz@gmail.com>
2017-04-06 21:18:58 -0500

    Updated to Tambera's specifications!

--- public/scripts.js
+++ public/scripts.js
@@ -12,7 +12,7 @@ $(function(){
 	$('#skillWeb').click(function(){
 		if (isrunning == false){
 		$('#info').text("");
-		information("I know my fair share about website creation! I know how to work with html to organize the necessary tags in order to fufill the requirements of the website. I know how to insert images, videos, add click and drag functionality, and many other functions. I also know a lot of CSS and JavaScript. I've organized this particular page with a lot of CSS and some JavaScript functionality (well, specifically jQuerry). I am pretty good at forms too!", 0);
+		information("I know how to work with html to organize the necessary tags in order to fufill the requirements of the website. I know how to insert images, videos, add click and drag functionality, and many other functions. I also know a lot of CSS and JavaScript. I've organized this particular page with a lot of CSS and some JavaScript functionality.", 0);
 		}
 		else{
 		isrunning = false;
@@ -25,7 +25,7 @@ $(function(){
 		if (isrunning == false){
 			isrunning == true;
 			$('#info').text("");
-		information("I'm fairly good at programming in C#. I know my way around LINQ, creating classes and instances of those classes, creating relavant fields for those objects, and putting it all to good use! I'm good at working with linked lists, too; however I prefer using databases when it gets to be like that! In class, I created a little banking application where you could create new accounts, deposit/withdrawl money, determine account time, gather required info(such as ID, address, etc.), and create bills to pay directly from the account. No real money was used during the process.", 0);
+		information("I'm great at programming in C#. I know my way around LINQ, creating classes and instances of those classes, creating relavant fields for those objects, and putting it all to good use! I'm good at working with linked lists, too; however I prefer using databases when it gets to be like that! In class, I created a little banking application where you could create new accounts, deposit/withdrawl money, determine account time, gather required info(such as ID, address, etc.), and create bills to pay directly from the account. No real money was used during the process.", 0);
 		}
 		else{
 			isrunning = false;
@@ -37,7 +37,7 @@ $(function(){
 		if (isrunning == false){
 			isrunning == true;
 			$('#info').text("");
-		information("I have experience working with Microsoft SQL Server and Oracle Databases from the classes I took in college. I learned how to create a query to create, update, or drop tables. I also know how to use T-SQL. I can insert data into those tables, remove data from tables, and create audit trails in order to see the modification history of a table. I also learned how to work with MySQL and PostgreSQL in my spare time in college.", 0);
+		information("I have experience working with Microsoft SQL Server and Oracle Databases from the classes I took in college. I learned how to create a query to create, update, or drop tables. I also know how to use T-SQL. I can insert data into those tables, remove data from tables, and create audit trails in order to see the modification history of a table. I also learned how to work with MySQL and PostgreSQL in my spare time.", 0);
 		}
 		else{
 			isrunning = false;
@@ -61,7 +61,7 @@ $(function(){
 		if (isrunning == false){
 			isrunning == true;
 			$('#info').text("");
-		information("In class, we mostly worked with Objective-C and Swift. We didn't get to work with X-Code due to not having Macs at the college. I still believe that with a little guidance, I will be able to create applications with X-Code. I have watched some Youtube videos on it so that I can be a little better prepared if I need to create applications for Apple devices.", 0);
+		information("In class, we worked with Objective-C and Swift. We installed Swift for Ubuntu and got to work. We also used JDoodle to interperet Objective-C", 0);
 		}
 		else{
 			isrunning = false;
@@ -73,7 +73,7 @@ $(function(){
 		if (isrunning == false){
 			isrunning == true;
 			$('#info').text("");
-		information("In my free time, I've done some learning on GIT. We didn't learn this in class; however, it seems pretty easy to work with. I can initialize git, add all files to the location, push it, and pull it. I can also add messages to comment what changes have been made. I also know how to obtain an SSH certificate and push to a remote server such as Heroku.", 0);
+		information("I've done some research on GIT. We didn't learn this in class; however, it seems pretty easy to work with. I can initialize git, add all files to the location, push it, roll back changes, and clone from a repository. I can also add messages to comment what changes have been made. I also know how to obtain an SSH certificate and push to a remote server such as Github or Heroku.", 0);
 		}
 		else{
 			isrunning = false;
@@ -85,7 +85,7 @@ $(function(){
 		if (isrunning == false){
 			isrunning == true;
 			$('#info').text("");
-		information("Ruby on Rails is one of the more fun things I've worked on. I followed a free Udemy cource on the creation of a Rails server, as well as working with Ruby. I really enjoyed it as it helped shine a little light on how blogs are run.", 0);
+		information("Ruby on Rails is one of the more fun things I've worked on. I created this website using Ruby On Rails! I really enjoyed it as it helped shine a little light on how blogs and dynamic websites are able to be run.", 0);
 		}
 		else{
 			isrunning = false;
